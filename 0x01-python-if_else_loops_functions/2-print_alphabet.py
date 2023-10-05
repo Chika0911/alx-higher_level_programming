18deffec3fd210919033df40f23a36b2ac7e7ff8
Chika0911 <135050146+Chika0911@users.noreply.github.com>
2023-10-05 01:47:42 -0700

    Update 2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-for char in range(97, 123):
-    print("{:c}".format(char), end='')
+# 2-print_alphabet.py
+for letter in range(97, 123):
+    print("{}".format(chr(letter)), end="")
